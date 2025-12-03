e56f2fb707622d5ca15ce281150643a798a3c625
Kanchan1015 <akanchan0315@gmail.com>
2025-12-03 14:14:29 +0530

    commented unused dec

--- backend/src/main/java/com/trello/backend/auth/jwt/JwtService.java
+++ backend/src/main/java/com/trello/backend/auth/jwt/JwtService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
-import java.util.UUID;
+// import java.util.UUID;
 
 @Service
 public class JwtService {
